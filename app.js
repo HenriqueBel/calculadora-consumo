394bc503f9656556018224a0822a42cfe207898c
henrique.bel <henriquebelmonte2008@hotmail.com>
2025-10-17 16:43:14 -0300

    ajustando o formulario para add consumo em litros, distancia e tempo

--- app.js
+++ app.js
@@ -26,14 +26,18 @@ viagem.consumoLitros = Math.round(viagem.percurso / veiculo.consumoMedio);
 combustivel.tipo = 0;
 
 if (combustivel.tipo === 0) {
-  viagem.custoEmReais = (viagem.consumoLitros * combustivel.precoEtanol).toLocaleString("pt-BR" , {
-    style:"currency", 
-    currency: "BRL"
+  viagem.custoEmReais = (
+    viagem.consumoLitros * combustivel.precoEtanol
+  ).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   });
 } else {
-  viagem.custoEmReais = (viagem.consumoLitros * combustivel.precoGasolina).toLocaleString("pt-BR" , {
-    style:"currency", 
-    currency: "BRL"
+  viagem.custoEmReais = (
+    viagem.consumoLitros * combustivel.precoGasolina
+  ).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   });
 }
 
--- scrips.js
+++ scrips.js
@@ -40,16 +40,60 @@ elemento.formulario.addEventListener("submit", (evento) => {
   calcularConsumo();
 });
 
+
+
+
+
 function calcularConsumo() {
+  //VARIAVEIS PARA CAPTURAR O VALOR DA HORA E MINUTO
   viagem.duracao = input.duracao.value;
   viagem.velocidademedia = input.velocidade.value;
 
-  let hora = viagem.duracao.slice(0,2)
-  let minuto = viagem.duracao.slice(3)
+  //CAPTURA O VALOR DO CONSUMO MEDIO DO VEICULO DIGITADO PELO USUARIO
+  veiculo.consumoMedio = input.consumo.value
 
-  console.log(hora, minuto);
+
+  console.log( veiculo.consumoMedio)
+
+  //MÉTODO(SLICE) PARA CORTAR O SÍMBOLO DE ":" DA HORA E MINUO, SEPARANDO EM VARIAVEIS PROPRIAS
+  let hora = +viagem.duracao.slice(0,2)
+  let minuto = +viagem.duracao.slice(3)
+
+  // FÓRMULA PARA CALCULAR A DISTANCIA PERCORRIDA PELO USUARIO
+  viagem.percurso  = (viagem.velocidademedia *((hora*60 + minuto)/60)). toFixed(1)
+
+  //METODO (REPLACE) PARA SUBSTITUIR "." POR "," NA EXIBIÇÃO DA DISTANCIA TOTAL
+  console.log(viagem.percurso  .replace(".",",") + " Km")
+
+  //CALCULO DO CONSUMO EM LITROS GASTOS NA VIAGEM
+  viagem.consumoLitros = viagem.percurso / veiculo.consumoMedio
+
+  console.log(viagem.consumoLitros)
+
+  
+const paragrafo = document.querySelector(".resultado")
+
+  //CALCULO PARA SABER O CUSTO EM REAIS (R$) DE ACORDO COM O CONSUMO EM LITROS
+
+if(combustivel.tipo.toLocaleLowerCase()=== "etanol"){
+  viagem.custoEmReais = viagem.consumoLitros * combustivel.precoEtanol
+
+  paragrafo.innerText = `Custo do Consumo em Reais: ${viagem.custoEmReais.toLocaleString("pt-BR" , {
+    style: "currency" ,
+    currency: "BRL"
+  })}  Percorreu: ${viagem.percurso.replace(".",",")} KM e abasteceu ${viagem.consumoLitros.toFixed(2)} Litros`
+
+  //METODO PARA FORMATAR COMO MOEDA(R$)
+  
+} else {viagem.custoEmReais = viagem.consumoLitros * combustivel.precoGasolina
+
+ //METODO PARA FORMATAR COMO MOEDA(R$)
+ paragrafo.innerText = `Custo da Gasolina: ${viagem.custoEmReais.toLocaleString("pt-BR" , {
+    style: "currency" ,
+    currency: "BRL"
+})} Percorreu: ${viagem.percurso.replace(".",",")} KM e abasteceu ${viagem.consumoLitros.toFixed(2)} Litros`
+}     
+
+
+
 }
-
-viagem.percurso = viagem.duracao * viagem.velocidademedia;
-
-viagem.consumoLitros = Math.round(viagem.percurso / veiculo.consumoMedio);
